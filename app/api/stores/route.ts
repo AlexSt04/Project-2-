1ceef5de3f4037daaf89a37ac5a543023455605e
AlexSt04 <alex.3stoica@gmail.com>
2025-09-16 00:14:26 +0300

    Prisma, Railway, MySQL setup

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { auth } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 
@@ -36,3 +37,38 @@ export async function POST(req: Request) {
     return new NextResponse('Internal Server Error', { status: 500 })
   }
 }
+=======
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(
+    req: Request,
+) {
+    try{
+      const { userId } = await auth();
+      const body = await req.json();
+      const { name } = body;
+
+      if(!userId) {
+        return new NextResponse("Unauthorized", {status: 401});
+      }
+
+      if(!name) {
+        return new NextResponse("Name is required", {status: 400});
+      }
+
+      const store = await prismadb.store.create({
+        data: {
+            name,
+            userId
+        }
+      })
+
+      return NextResponse.json(store);
+    } catch(error){
+        console.log('[STORES_POST]', error);
+        return new NextResponse("Internal error", {status: 500});
+    }
+}
+>>>>>>> d2bdfd4 (Prisma, Railway, MySQL setup)
--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { PrismaClient } from '@prisma/client'
 
 declare global {
@@ -9,3 +10,15 @@ const prismadb = globalThis.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismadb
 
 export default prismadb
+=======
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    var prisma: PrismaClient | undefined
+};
+
+const prismadb = globalThis.prisma || new PrismaClient();
+if  (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+export default prismadb;
+>>>>>>> d2bdfd4 (Prisma, Railway, MySQL setup)
